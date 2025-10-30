fb643788201c287495616331d69f0d3f1559c442
Renju T. R <renju.t@ferax.com>
2025-10-30 17:52:21 +0530

    Initial commit

--- components/lib/components/buttons/f_t_button_back/f_t_button_back_widget.dart
+++ components/lib/components/buttons/f_t_button_back/f_t_button_back_widget.dart
@@ -52,17 +52,18 @@ class _FTButtonBackWidgetState extends State<FTButtonBackWidget> {
   Widget build(BuildContext context) {
     return FlutterFlowIconButton(
       borderColor: Colors.transparent,
-      borderRadius: 30.0,
-      borderWidth: 1.0,
+      borderRadius: 30,
+      borderWidth: 1,
       hoverIconColor: FlutterFlowTheme.of(context).primary,
       icon: Icon(
         Icons.arrow_back_rounded,
         color: FlutterFlowTheme.of(context).secondaryText,
-        size: 30.0,
+        size: 30,
       ),
       onPressed: () async {
-        Navigator.pop(context);
+        await widget.onPressed?.call();
       },
     );
   }
 }
+
